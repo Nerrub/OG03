a689b5ce6ec03d28126da07c9d9b96ae092942d9
Nerrub <Lyapin_dmitriy@hotmail.com>
2024-05-06 13:39:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 pygame.init()
 
+SCREEN_WIGHT=800
+SCREEN_HEIGHT=600
+screen=pygame.display.set_mode((SCREEN_WIGHT, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра тир")
+icon=pygame.image.load("")
 running= True
 
 while running:
