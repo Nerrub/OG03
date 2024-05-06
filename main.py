1ed4ae6cb9bd441b7639d88537023adec6ac0557
Nerrub <Lyapin_dmitriy@hotmail.com>
2024-05-06 13:51:37 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 SCREEN_WIGHT=800
@@ -6,7 +7,18 @@ SCREEN_HEIGHT=600
 screen=pygame.display.set_mode((SCREEN_WIGHT, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра тир")
-icon=pygame.image.load("")
+icon=pygame.image.load("img/4c9255d3-aade-4db4-87d2-9054fd5d4093.jpg")
+pygame.display.set_icon(icon)
+
+target_img=pygame.image.load("img/klipartz.com(1).png")
+target_weight=80
+target_height=80
+
+target_x=random.randint(0,SCREEN_WIGHT-target_weight)
+target_y=random.randint(0,SCREEN_HEIGHT-target_height)
+
+color=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
 running= True
 
 while running:
